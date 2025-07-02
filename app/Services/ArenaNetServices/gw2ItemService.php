d405586bf2cb69668815cd2a02339697b342fa08
Willian Santos Horacio <williansh6e@hotmail.com>
2025-07-02 17:04:08 -0300

    Criada nova classe de exception para a api. Aplicado retry nas requests

--- app/Services/ArenaNetServices/gw2ItemService.php
+++ app/Services/ArenaNetServices/gw2ItemService.php
@@ -10,6 +10,7 @@
     namespace App\Services;
 
     use Illuminate\Support\Facades\Http;
+    use App\Exceptions\Gw2ApiException;
 
     class Gw2ItemService
     {
@@ -22,19 +23,19 @@
 
         public function getAllFinishers() 
         {
-            $response = Http::get("{$this->baseUrl}/finisher");
+            $response = $this->http()->get("{$this->baseUrl}/finisher");
             return $this->handleResponse($response);
         }
 
         public function getFinisher(int $id) 
         {
-            $response = Http::get("{$this->baseUrl}/finisher/{$id}");
+            $response = $this->http()->get("{$this->baseUrl}/finisher/{$id}");
             return $this->handleResponse($response);
         }
 
         public function getFinishers(array $ids)
         {
-            $response = Http::get("{$this->baseUrl}/finisher", [ "ids" => implode(',', $ids)]);
+            $response = $this->http()->get("{$this->baseUrl}/finisher", [ "ids" => implode(',', $ids)]);
             return $this->handleResponse($response);
         }
 
@@ -44,19 +45,19 @@
 
         public function getItem(int $id)
         {
-            $response = Http::get("{$this->baseUrl}/items/{$id}");
+            $response = $this->http()->get("{$this->baseUrl}/items/{$id}");
             return $this->handleResponse($response);
         }
 
         public function getItems(array $ids)
         {
-            $response = Http::get("{$this->baseUrl}/items", [ "ids" => implode(',', $ids)]);
+            $response = $this->http()->get("{$this->baseUrl}/items", [ "ids" => implode(',', $ids)]);
             return $this->handleResponse($response);
         }
 
         public function getAllItems()
         {
-            $response = Http::get("{$this->baseUrl}/items");
+            $response = $this->http()->get("{$this->baseUrl}/items");
             return $this->handleResponse($response);
         }
 
@@ -64,21 +65,21 @@
 // │                            Itemstats Endpoints                              │
 // └─────────────────────────────────────────────────────────────────────────────┘
 
-        public function getItemStats($id)
+        public function getItemStats(int $id)
         {   
-            $response = Http::get("{$this->baseUrl}/itemstats/{$id}");
+            $response = $this->http()->get("{$this->baseUrl}/itemstats/{$id}");
             return $this->handleResponse($response);
         }
 
-        public function getItemsStats($ids)
+        public function getItemsStats(array $ids)
         {
-            $response = Http::get("{$this->baseUrl}/itemstats", ["ids" => implode(',', $ids)]);
+            $response = $this->http()->get("{$this->baseUrl}/itemstats", ["ids" => implode(',', $ids)]);
             return $this->handleResponse($response);
         }
 
         public function getAllItemStats()
         {
-            $response = Http::get("{$this->baseUrl}/itemstats");
+            $response = $this->http()->get("{$this->baseUrl}/itemstats");
             return $this->handleResponse($response);
         }
 
@@ -88,13 +89,13 @@
 
         public function getMaterialCategory(int $id)
         {
-            $response = Http::get("{$this->baseUrl}/materials/{$id}");
+            $response = $this->http()->get("{$this->baseUrl}/materials/{$id}");
             return $this->handleResponse($response);
         }
 
         public function getAllMaterialCategorys()
         {
-            $response = Http::get("{$this->baseUrl}/materials");
+            $response = $this->http()->get("{$this->baseUrl}/materials");
             return $this->handleResponse($response);
         }
 
@@ -104,7 +105,7 @@
 
         public function getPvpAmulet(int $id)
         {
-            $response = Http::get("{$this->baseUrl}/pvp/amulets/{$id}");
+            $response = $this->http()->get("{$this->baseUrl}/pvp/amulets/{$id}");
             return $this->handleResponse($response);
         }
 
@@ -115,7 +116,7 @@
                 'page_size' => $pageSize > 0 ? $pageSize : null,
             ]);
 
-            $response = Http::get("{$this->baseUrl}/pvp/amulets", $params);
+            $response = $this->http()->get("{$this->baseUrl}/pvp/amulets", $params);
             return $this->handleResponse($response);
         }
 
@@ -125,13 +126,13 @@
 
         public function getRecipe(int $id)
         {
-            $response = Http::get("{$this->baseUrl}/recipes/{$id}");
+            $response = $this->http()->get("{$this->baseUrl}/recipes/{$id}");
             return $this->handleResponse($response);
         }
 
         public function getAllRecipes()
         {
-            $response = Http::get("{$this->baseUrl}/recipes");
+            $response = $this->http()->get("{$this->baseUrl}/recipes");
             return $this->handleResponse($response);
         }
 
@@ -147,38 +148,47 @@
             ]);
 
             if (!empty($params)) {
-                $response = Http::get("{$this->baseUrl}/recipes/search", $params);
+                $response = $this->http()->get("{$this->baseUrl}/recipes/search", $params);
                 return $this->handleResponse($response);
             }
 
             throw new \Exception('No params informed on getRecipeSearch endpoint');
         }
 
-
-        public function getLegendaryArmory()
-        {
-            $response = Http::get("{$this->baseUrl}/legendaryarmory");
-            return $this->handleResponse($response);
-        }
+// ┌─────────────────────────────────────────────────────────────────────────────┐
+// │                               Skins Endpoints                               │
+// └─────────────────────────────────────────────────────────────────────────────┘
 
         public function getSkin(int $id)
         {
-            $response = Http::get("{$this->baseUrl}/skins/{$id}");
+            $response = $this->http()->get("{$this->baseUrl}/skins/{$id}");
             return $this->handleResponse($response);
         }
 
-        public function getItemRecipe(int $id)
+        public function getAllSkins()
         {
-            $response = Http::get("{$this->baseUrl}/recipes/{$id}");
+            $response = $this->http()->get("{$this->baseUrl}/skins");
             return $this->handleResponse($response);
         }
+
+// ┌─────────────────────────────────────────────────────────────────────────────┐
+// │                             Auxiliar Functions                              │
+// └─────────────────────────────────────────────────────────────────────────────┘
 
         protected function handleResponse($response) 
         {
             if ($response->failed()) {
-                throw new \Exception('GW2 API Request failed: '.$response->status());
+                throw new Gw2ApiException(
+                    'GW2 API Request failed: ' . $response->status(),
+                    $response->status()
+                );
             }
 
             return $response->json();
+        }
+
+        protected function http()
+        {
+            return Http::retry(3, 100);
         }
     }
