173d06f5114a7f97109229b702edea558b8a120b
Willian Santos Horacio <williansh6e@hotmail.com>
2025-07-07 13:54:06 -0300

    save

--- app/Services/ArenaNetIntegrator/Gw2ItemSyncLogging.php
+++ app/Services/ArenaNetIntegrator/Gw2ItemSyncLogging.php
@@ -7,20 +7,42 @@ class Gw2ItemSyncLogging
 {
     public function writeInfo(string $message, array $context = []) {
         Log::channel('gw2sync')->info($message, $context);
+        echo $message . PHP_EOL;
     }
 
     public function writeError(string $message, array $context = []) {
         Log::channel('gw2sync')->error($message, $context);
+        echo $message . PHP_EOL;
     }
 
     public function writeDebug(string $message, array $context = []) {
         Log::channel('gw2sync')->debug($message, $context);
+        echo $message . PHP_EOL;
     }
 
     public function writeHeader(string $title)
     {
         $line = str_repeat('-', 60);
         $formatted = "\n$line\n>>> $title <<<\n$line";
-        Log::channel('gw2sync')->info($formatted);
+        $this->writeInfo($formatted);
+    }
+ 
+    public function writeSubHeader(string $title)
+    {
+        $line = str_repeat('=', 40);
+        $formatted = "\n$line\n>> $title <<\n$line\n";
+        $this->writeInfo($formatted);
+    }
+
+    public function writeItem(string $message)
+    {
+        $formatted = "  -> [{$message}]";
+        $this->writeInfo($formatted);
+    }
+
+    public function writeStartingChunk($actualChunk, $totalChunk)
+    {
+        $formatted = "Starting Chunk {$actualChunk} of {$totalChunk}";
+        $this->writeSubHeader($formatted);
     }
 }
--- app/Services/ArenaNetIntegrator/Gw2ItemSyncService.php
+++ app/Services/ArenaNetIntegrator/Gw2ItemSyncService.php
@@ -2,9 +2,17 @@
 
 namespace App\Services\ArenaNetIntegrator;
 
-use App\Models\GameItem;
+use App\Models\{
+    GameDetail,
+    GameItem,
+    GameItemFlag,
+    GameItemGameType,
+    GameItemRestriciton
+};
+
 use App\Services\ArenaNetServices\Gw2ItemService;
 use App\Services\ArenaNetIntegrator\Gw2ItemSyncLogging;
+
 
 class Gw2ItemSyncService
 {
@@ -12,7 +20,9 @@ class Gw2ItemSyncService
     protected $itemService;
     protected $logging;
     protected array $itemIdsChunks;
-    protected array $itemsIdsWithError;
+    protected array $itemsIdsWithErrors;
+    protected int $totalChunks;
+    protected int $progress;
 
     public function __construct(Gw2ItemService $itemService, Gw2ItemSyncLogging $logging)
     {
@@ -26,17 +36,41 @@ class Gw2ItemSyncService
         $ids = $this->itemService->getAllItems();
 
         if (count($ids) > 0) {
-            $this->parseItems($ids);
+            $chunks = $this->prepareChunks($ids);
+            $this->processChunks($chunks);
         } else {
             $this->logging->writeHeader("SYNCHRONIZATION COMPLETED. NO ITEMS WERE RETURNED FROM THE GAME SERVER!");
         }
     }
 
-    protected function parseItems(array $ids)
+    protected function prepareChunks(array $ids)
     {
-        $chunks = array_chunk($ids, 100);
-        $totalChunks = count($chunks);
-        $this->logging->writeInfo("{$totalChunks} chunks prepared. Starting download...");
+        $chunks = array_chunk($ids, 50);
+        $this->totalChunks = count($chunks);
+        $this->logging->writeInfo(count($chunks)." chunks prepared. Starting download...");
+        return $chunks;
+    }
+
+    protected function processChunks(array $chunks)
+    {
+        $this->progress = 0;
+        foreach ($chunks as $chunk) {
+            $this->progress++;
+            $this->logging->writeSubHeader("Starting Chunk {$this->progress} of {$this->totalChunks}");
+            foreach ($chunk as $item) {
+                $this->logging->writeItem($item);
+                $itemData = $this->itemService->getItems($chunk);
+                if($itemData) {
+                    $this->processItemRelationshipData($itemData);
+                }
+            }
+        }
+    }
+
+    protected function processItemRelationshipData(array $itemData)
+    {
+        //Aqui serão baixados os relacionamtos dos itens, como gametypes (preecher tabela pivot), flag, restrictons, status, coisas que precisam de mais consultas de endpoint
+        //Depois aplicarei o solid, removendo daqui metodos que dão mais de uma responsabilidade para esta classe, e injetando aqui
     }
 
 }
